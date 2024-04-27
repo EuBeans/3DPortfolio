51ff832a15c0c81b37c5d91c95573610583e6a4d
Jean Sfeir <43392082+EuBeans@users.noreply.github.com>
2024-04-27 18:45:16 -0400

    compress more

--- compressModels.js
+++ compressModels.js
@@ -26,8 +26,7 @@ folders.forEach(folder => {
     if (path.extname(file) === '.glb' && !file.startsWith('compressed_')) {
       const inputPath = path.join(folderPath, file);
       const outputPath = path.join(folderPath, 'compressed_' + file);
-      const command = `gltf-pipeline -i ${inputPath} -o ${outputPath} -d --draco.quantizePositionBits 8 --draco.quantizeTexcoordBits 8 --draco.quantizeNormalBits 6 --draco.quantizeGenericBits 6`;
-      console.log(`Compressing: ${file} in folder ${folder}`);
+      const command = `gltf-pipeline -i ${inputPath} -o ${outputPath} -d --draco.quantizePositionBits 6 --draco.quantizeTexcoordBits 6 --draco.quantizeNormalBits 4 --draco.quantizeGenericBits 4`;      console.log(`Compressing: ${file} in folder ${folder}`);
       execSync(command, { stdio: 'inherit' });
       console.log(`Compressed: ${file} -> ${outputPath}`);
     }
